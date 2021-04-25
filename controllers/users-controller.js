83a3b44652263354a45bd0974ed197f21a981163
Kristin White <kristinwhite103@gmail.com>
2021-04-25 14:03:17 -0400

    update

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -34,7 +34,7 @@ module.exports = (app) => {
         req.session;
     });
 
-    app.post('api/users/current', (req, res) => {
+    app.get('api/users/current', (req, res) => {
         const currentUser = req.session["currentUser"];
         if(currentUser){
             res.send(currentUser)
