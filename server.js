ad6e71ad6aef098e1f4f49106a6d34aebfa7f355
Kristin White <kristinwhite103@gmail.com>
2021-04-25 18:53:06 -0400

    update

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.use(session({
         autoRemove: 'native'
     }),
     resave: false,
-    saveUninitialized: true
-    // cookie: { secure: true }
+    saveUninitialized: true,
+    cookie: {maxAge: 60000, secure: true}
 }));
 
 app.use(function (req, res, next) {
