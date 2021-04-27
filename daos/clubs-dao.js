f82112fe24d438d9606ae373b44247f9c74e17ec
tchen007 <46579382+tchen007@users.noreply.github.com>
2021-04-27 01:10:37 -0400

    updateClub

--- daos/clubs-dao.js
+++ daos/clubs-dao.js
@@ -1,11 +1,18 @@
 const clubsModel = require('../models/clubs/clubs-model');
+const ObjectID = require('mongodb').ObjectID;
 
 const findAllClubs = () => clubsModel.find();
 const findClubById = (clubId) => clubsModel.findById(clubId);
 const findClubsByTitle = (clubTitle) => clubsModel.find({title:clubTitle});
 const findClubsById = (clubIds) => clubsModel.find({_id: clubIds});
 const createClub = (club) => clubsModel.create(club);
-const updateClub = (club) => clubModel.updateOne({_id:club._id},club);
+const updateClub = (club) => clubModel.updateOne({_id: new ObjectID(club._id)},
+    {$set:
+            {
+                description: club.description,
+                pictureURL: club.pictureURL,
+                posts: club.posts
+            }});
 const deleteClub = (clubId) => clubModel.remove({_id: clubId}, {justOne: true});
 
 module.exports = {
