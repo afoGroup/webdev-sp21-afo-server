68559544d73616821011164937e496e2b965b7f2
tchen007 <46579382+tchen007@users.noreply.github.com>
2021-04-27 00:17:47 -0400

    objectID

--- daos/users-dao.js
+++ daos/users-dao.js
@@ -14,7 +14,18 @@ const createUser = (newUser) =>
     usersModel.create(newUser);
 
 const updateUser = (user) =>
-    usersModel.updateOne({_id: new ObjectID(user._id)}, {$set: user});
+    usersModel.updateOne({_id: new ObjectID(user._id)},
+        {$set:
+                {
+                    password: user.password,
+                    userType: user.userType,
+                    email: user.email,
+                    bio: user.bio,
+                    instagram: user.instagram,
+                    twitter: user.twitter,
+                    pictureURL: user.pictureURL
+                },
+        });
 
 const deleteUser = (userId) =>
     usersModel.remove({_id: userId});
